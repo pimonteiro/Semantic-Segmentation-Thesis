97a1b5237225de4e03600ca24777a85683eb5f70
Filipe Monteiro <pimonteiro@protonmail.com>
2021-05-25 17:42:34 +0100

    Add docker support

---
+++ run_container.sh
@@ -0,0 +1,5 @@
+docker run -it --gpus all --rm -p "8888:8888" -p "6006:6006" -v "/home/pimonteiro/repos/Semantic-Segmentation-Thesis/:/workspace/:Z" -v "/mnt/7BCDA59C6DEFFE3C/:/mnt/7BCDA59C6DEFFE3C/" my_ml_container bash
+
+# jupyter notebook --port=8888 --ip=0.0.0.0 --allow-root --no-browser .
+# tensorboard --logdir --bind_all
+# sudo docker exec -it <name> bash
